2df3b1dab250cd0e10ab50a384617d7b75c1328a
Ares <114934460+Aries2337@users.noreply.github.com>
2024-06-20 14:00:42 +0100

    Update DeployFundMe.s.sol

--- solidity-course/foundry-f23/foundry-fund-me-f23/script/DeployFundMe.s.sol
+++ solidity-course/foundry-f23/foundry-fund-me-f23/script/DeployFundMe.s.sol
@@ -9,7 +9,7 @@ import {HelperConfig} from "./HelperConfig.s.sol";
 contract DeployFundMe is Script {
     function run() external returns (FundMe) {
 
-        HelperConfig helperConfig = new HelperConfig(); // this will create a new contract called `new  HelperConfig` this will allow us to test it first before we delpoy it on a real network as it costs gas so anything before `vm.startBroadcast` will not be sent as a real transaction ---- anything after `vm.Broadcast` will be sent as a real transaction and cost gas
+        HelperConfig helperConfig = new HelperConfig(); 
 
 
         address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
